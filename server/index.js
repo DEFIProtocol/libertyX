218f248d15c1169b2d3599a2bad240b77f22a65f
DEFIProtocol <Beauallgood@gmail.com>
2026-02-15 20:48:18 -0600

    update pool postgres var

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ const PORT = process.env.PORT || 3001;
 
 // PostgreSQL connection
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'gridlockdb',
-    password: process.env.DB_PASSWORD || 'asdas',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
 });
 
